3af3bdb9b0d639cfb2039d7546dbd8d90e56683d
Alexandre Silva <thekaway404@gmail.com>
2023-07-12 17:26:21 -0300

    feat: header

--- frontend/src/app/header/index.tsx
+++ frontend/src/app/header/index.tsx
@@ -3,12 +3,14 @@ import { useState, useEffect } from "react";
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 import './index.css';
-import expand from "../../resources/images/expandgrey.png"
+import expand_icon from "../../resources/images/expandgrey.png"
+import bell_icon from "../../resources/images/bell.png"
+import chat_icon from "../../resources/images/chat.png"
 import search_icon from "../../resources/images/search.svg";
 
 import { Link } from "react-router-dom";
 
-const typePage = "dashboard";
+const typePage = "header";
 
 declare global {
     interface Window {
@@ -27,9 +29,11 @@ function Header({ user }: any) {
             </div>
             <div className="absolute right">
                 <img className="icon search-input" src={search_icon}/>
-                <input className="search-bar" type="text" placeholder="Search"/>
+                <input className="search-bar" type="text" placeholder="Search on Shako"/>
+                <img className="icon bell-icon" src={bell_icon}/>
+                <img className="icon chat-icon" src={chat_icon}/>
                 <div className="profile">
-                    <img className="icon expand" src={expand}/>
+                    <img className="icon expand" src={expand_icon}/>
                     <img className="avatar" src="https://www.redditstatic.com/avatars/avatar_default_12_545452.png"/>
                     <span className="username">{user.username}#{user.discrimination}</span>
                 </div>
