79879e10c4e74d42bad14f29042af461b0c1e904
Alexandre Silva <thekaway404@gmail.com>
2023-07-11 22:11:51 -0300

    fix

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -11,23 +11,9 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ user }) => {
   return (
-    <Toolbar className="header">
-      <div className="left">
-        
-      </div>
-      <div className="center">
-        <Link to="/" className="logo">
-          Tumblr
-        </Link>
-      </div>
-      <div className="right">
-        <img src={user.profileImage} alt="Profile" className="profile-image" />
-      </div>
-      <div className="search-bar">
-        <Input type="search" placeholder="Search Tumblr" className="search-input" />
-        <Icon icon="md-search" className="search-icon" />
-      </div>
-    </Toolbar>
+    <>
+    
+    </>
   );
 };
 
