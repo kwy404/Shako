44f1b5ad735a52ea8114c01103106cad2ee3d583
Alexandre Silva <thekaway404@gmail.com>
2023-07-12 09:00:02 -0300

    fix: typescript

--- frontend/src/components/Alert/index.tsx
+++ frontend/src/components/Alert/index.tsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { AlertDialog, Button } from 'react-onsenui';
 
-const MyAlertDialog: React.FC = (props:any) => {
+interface MyAlertDialogProps {
+  open: boolean;
+  message: string;
+  error: string;
+  setDialog: (value: boolean) => void;
+}
+
+const MyAlertDialog: React.FC<MyAlertDialogProps> = (props) => {
   const handleCancel = () => {
-    props?.setDialog(false);
+    props.setDialog(false);
   };
 
   const handleOpenDialog = () => {
-    props?.setDialog(true);
+    props.setDialog(true);
   };
 
   return (
     <>
-      <AlertDialog isOpen={props?.open} onCancel={handleCancel} cancelable>
-        <div className="alert-dialog-title">{props?.error}</div>
-        <div className="alert-dialog-content">{props?.message}</div>
+      <AlertDialog isOpen={props.open} onCancel={handleCancel} cancelable>
+        <div className="alert-dialog-title">{props.error}</div>
+        <div className="alert-dialog-content">{props.message}</div>
         <div className="alert-dialog-footer">
           <Button onClick={handleCancel} className="alert-dialog-button">
             Ok
