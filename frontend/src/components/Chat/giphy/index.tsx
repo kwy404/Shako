266ebf87a5661549148212cdb364de5b4d541ef3
kwy404 <thekaway404@gmail.com>
2023-08-06 14:48:41 -0300

    feat: cache gifs

--- frontend/src/components/Chat/giphy/index.tsx
+++ frontend/src/components/Chat/giphy/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Socket } from 'socket.io-client';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 import './index.css';
 
 interface Gif {
@@ -6,25 +8,42 @@ interface Gif {
   url: string;
 }
 
-import { Socket } from "socket.io-client";
-import { DefaultEventsMap } from 'socket.io/dist/typed-events';
-
 interface Props {
-  socket: Socket<DefaultEventsMap, DefaultEventsMap> | null,
-  emited: (data: any, type: string, socket: Socket<DefaultEventsMap, DefaultEventsMap>) => void,
-  selectUserId: string,
-  setGifOpen: (data: any) => void
+  socket: Socket<DefaultEventsMap, DefaultEventsMap> | null;
+  emited: (data: any, type: string, socket: Socket<DefaultEventsMap, DefaultEventsMap>) => void;
+  selectUserId: string;
+  setGifOpen: (data: any) => void;
 }
 
 const GifSelector = ({ socket, emited, selectUserId, setGifOpen }: Props) => {
   const [gifs, setGifs] = useState<Gif[]>([]);
-  const [searchQuery, setSearchQuery] = useState<string>(''); // State to store search query
-  const [presetUrls, setPresetUrls] = useState<string[]>([]); // State to store preset GIF URLs
-  const [searchResults, setSearchResults] = useState<Gif[]>([]); // State to store search results
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [presetUrls, setPresetUrls] = useState<string[]>([]);
+  const [searchResults, setSearchResults] = useState<Gif[]>([]);
+  const [cachedGifs, setCachedGifs] = useState<{ [query: string]: Gif[] }>({});
 
-  // Coloque a sua chave de API do Giphy aqui
   const API_KEY = 'R5ThJWeA3Hs6FhqupCRNpIOr9zG6L3ue';
   const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}`;
+
+  // Helper function to store data in localStorage
+  const storeInLocalStorage = (key: string, data: any) => {
+    try {
+      localStorage.setItem(key, JSON.stringify(data));
+    } catch (error) {
+      console.error('Error storing data in localStorage:', error);
+    }
+  };
+
+  // Helper function to retrieve data from localStorage
+  const getFromLocalStorage = (key: string) => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : null;
+    } catch (error) {
+      console.error('Error retrieving data from localStorage:', error);
+      return null;
+    }
+  };
 
   // Preset GIF options
   const presetGifs = [
@@ -55,42 +74,70 @@ const GifSelector = ({ socket, emited, selectUserId, setGifOpen }: Props) => {
   ];
 
   useEffect(() => {
-    // Fetch the first GIF for each preset and store the URLs in state
-    const fetchPresetUrls = async () => {
-      const urls = await Promise.all(
+    // Fetch preset URLs from localStorage if available
+    const cachedPresetUrlsFromStorage = getFromLocalStorage('cachedPresetUrls');
+    if (cachedPresetUrlsFromStorage) {
+      setPresetUrls(cachedPresetUrlsFromStorage as string[]);
+    } else {
+      fetchPresetUrls();
+    }
+
+    // Fetch preset GIFs from localStorage if available
+    const cachedGifsFromStorage = getFromLocalStorage('cachedGifs');
+    if (cachedGifsFromStorage) {
+      setCachedGifs(cachedGifsFromStorage as { [query: string]: Gif[] });
+    } else {
+      fetchPresetGifs();
+    }
+  }, []);
+
+  const fetchPresetUrls = async () => {
+    try {
+      const presetUrlsFromCache: { [preset: string]: string } = {};
+      await Promise.all(
         presetGifs.map(async (preset) => {
           const response = await fetch(`${API_URL}&q=${preset}&limit=1`);
           const data = await response.json();
           const gif = data.data[0];
-          return gif ? gif.images.fixed_height.url : '';
+          const url = gif ? gif.images.fixed_height.url : '';
+          presetUrlsFromCache[preset] = url;
         })
       );
-      setPresetUrls(urls);
-    };
 
-    fetchPresetUrls();
-  }, []);
+      setPresetUrls(presetGifs.map((preset) => presetUrlsFromCache[preset]));
 
-  // Function to fetch GIFs from Giphy
-  const fetchGifs = async (query: string) => {
-    try {
-      const response = await fetch(`${API_URL}&q=${query}&limit=10`); // Limit to one GIF
-      const data = await response.json();
-      const gifs = data.data.map((gif: any) => ({
-        id: gif.id,
-        url: gif.images.fixed_height.url,
-      }));
-      setGifs(gifs);
-      setSearchQuery(query); // Set the search query when preset is clicked
+      // Store preset URLs in localStorage
+      storeInLocalStorage('cachedPresetUrls', presetUrlsFromCache);
     } catch (error) {
-      console.error('Error fetching GIFs:', error);
+      console.error('Error fetching preset GIF URLs:', error);
     }
   };
 
-  // Function to fetch GIFs from Giphy
+  const fetchPresetGifs = async () => {
+    try {
+      const gifsFromCache: { [query: string]: Gif[] } = {};
+      await Promise.all(
+        presetGifs.map(async (preset) => {
+          const response = await fetch(`${API_URL}&q=${preset}&limit=1`); // Limit to 1 GIF only
+          const data = await response.json();
+          const gif = data.data[0];
+          const url = gif ? gif.images.fixed_height.url : '';
+          gifsFromCache[preset] = [{ id: gif.id, url }]; // Store as an array with a single GIF
+        })
+      );
+
+      setCachedGifs(gifsFromCache);
+
+      // Store preset GIFs in localStorage
+      storeInLocalStorage('cachedGifs', gifsFromCache);
+    } catch (error) {
+      console.error('Error fetching preset GIFs:', error);
+    }
+  };
+
   const fetchGifsSearch = async (query: string) => {
     try {
-      const response = await fetch(`${API_URL}&q=${query}&limit=10`); // Limit to ten GIFs
+      const response = await fetch(`${API_URL}&q=${query}&limit=10`);
       const data = await response.json();
       const gifs = data.data.map((gif: any) => ({
         id: gif.id,
@@ -102,30 +149,27 @@ const GifSelector = ({ socket, emited, selectUserId, setGifOpen }: Props) => {
     }
   };
 
-  const generateToken = (length: any) => {
-    const characters =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    let token = "";
+  const generateToken = (length: number) => {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    let token = '';
     for (let i = 0; i < length; i++) {
       const randomIndex = Math.floor(Math.random() * characters.length);
       token += characters[randomIndex];
     }
     return token;
-  }
+  };
 
-  // Function to handle GIF click and set as search query
   const handleGifClick = (url: string) => {
     if (!socket) {
       return;
     }
-    //Emited message
     emited(
       {
         usernameId: selectUserId,
         type: 'chatMessage',
-        token: window.localStorage.getItem('token') ? window.localStorage.getItem('token') : '',
+        token: window.localStorage.getItem('token') || '',
         message: `${url.split('.gif?')[0]}.gif`,
-        id: generateToken(20)
+        id: generateToken(20),
       },
       'chatContainer',
       socket
@@ -144,17 +188,20 @@ const GifSelector = ({ socket, emited, selectUserId, setGifOpen }: Props) => {
           fetchGifsSearch(e.target.value);
         }}
       />
-      {searchQuery.trim().length === 0 && (
+      {searchQuery.trim().length === 0 && getFromLocalStorage('cachedPresetUrls') && (
         <div className="preset-options gifs">
           {presetGifs.map((preset, index) => (
             <div className="gift" key={preset}>
-              <div onClick={() => {
-                setSearchQuery(preset);
-                fetchGifsSearch(preset);
-              }} className="overlaygif">
+              <div
+                onClick={() => {
+                  setSearchQuery(preset);
+                  fetchGifsSearch(preset);
+                }}
+                className="overlaygif"
+              >
                 <span>{preset}</span>
               </div>
-              <img src={presetUrls[index]} alt={preset} />
+              <img src={getFromLocalStorage('cachedPresetUrls')[preset]} alt={preset} onClick={() => handleGifClick(presetUrls[index])} />
             </div>
           ))}
         </div>
@@ -162,21 +209,11 @@ const GifSelector = ({ socket, emited, selectUserId, setGifOpen }: Props) => {
       <div className="gifs">
         {searchQuery.trim().length > 0
           ? searchResults.map((gif) => (
-            <img
-              key={gif.id}
-              src={gif.url}
-              alt="GIF"
-              onClick={() => handleGifClick(gif.url)}
-            />
-          ))
+              <img key={gif.id} src={gif.url} alt="GIF" onClick={() => handleGifClick(gif.url)} />
+            ))
           : gifs.map((gif) => (
-            <img
-              key={gif.id}
-              src={gif.url}
-              alt="GIF"
-              onClick={() => handleGifClick(gif.url)}
-            />
-          ))}
+              <img key={gif.id} src={gif.url} alt="GIF" onClick={() => handleGifClick(gif.url)} />
+            ))}
       </div>
     </div>
   );
