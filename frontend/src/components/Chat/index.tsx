98c97e1a4e99d617f4570a49f348f2cd369cdcde
kwy404 <thekaway404@gmail.com>
2023-11-26 14:58:29 -0300

    fix

--- frontend/src/components/Chat/index.tsx
+++ frontend/src/components/Chat/index.tsx
@@ -434,7 +434,7 @@ function ChatComponent({ user, emited, socket, setUser, handleAddNotification }:
     { selectUser.id && isOpen && 
     <div className={`right--profile--selected ${isOpen ? 'chat--component--max--' : 'chat--component--min--'}`}>
       {!selectUser.botYee && <Dashboard isProfile={true} setUser={setUser} user={user} chatProfile={{user_id: selectUser.id, username: selectUser.username, discrimination: selectUser.discrimination}}/> }
-      {selectUser.botYee && <h1>Bots no have profile include, maybe in the future.</h1>}
+      {selectUser.botYee && <h4>Bots no have profile include, maybe in the future.</h4>}
     </div> }
     { !selectUser.id && isOpen && 
     <div className={`right--profile--selected ${isOpen ? 'chat--component--max--' : 'chat--component--min--'}`}>
--- server/ia/chat.js
+++ server/ia/chat.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 // Função para obter resposta do bot usando a API do ChatGPT
 async function obterRespostaDoBot(pergunta) {
   // Chave de API da OpenAI (substitua pela sua chave)
-  const apiKey = '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';
+  const apiKey = 'sk-eionFWpNThMNy4eeFdC25789F60a4cC2A66b2c94D3948bA6';
   try {
     const resposta = await axios.post(
       'https://api.openai.com/v1/chat/completions',
