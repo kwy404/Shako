6ed17bacd68de06a881a13528062b68b61c0c99a
kwy404 <thekaway404@gmail.com>
2023-11-26 15:04:57 -0300

    new key chatgpt

--- server/ia/chat.js
+++ server/ia/chat.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 // Função para obter resposta do bot usando a API do ChatGPT
 async function obterRespostaDoBot(pergunta) {
   // Chave de API da OpenAI (substitua pela sua chave)
-  const apiKey = 'sk-eionFWpNThMNy4eeFdC25789F60a4cC2A66b2c94D3948bA6';
+  const apiKey = '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';
   try {
     const resposta = await axios.post(
       'https://api.openai.com/v1/chat/completions',
